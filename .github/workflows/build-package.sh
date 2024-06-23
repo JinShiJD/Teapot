efbcaf0ff5083342002fc94988706dad2577ea45
JinShiJD <81009367+JinShiJD@users.noreply.github.com>
2024-06-23 22:29:22 +0800

    Update build-package.sh

--- .github/workflows/build-package.sh
+++ .github/workflows/build-package.sh
@@ -23,13 +23,16 @@ Package: teapot
 Version: 1.1.1
 Architecture: amd64  # Adjust architecture as per your project
 Maintainer: JinShiJD <2282575601@qq.com>
-Depends: qt5-default, other-dependencies
-Description: Short description of your Qt package
+Depends: qt5-default, freeglut3, other-dependencies
+Description: Teapot Qt application
  Long description of your Qt package
 EOF
 
 # Install your Qt project into the correct directories within build/
 # Example: cp -r bin/ build/usr/bin
+
+mkdir -p build/usr/local/bin
+cp build/teapot build/usr/local/bin/
 
 # Build the package
 dpkg-deb --build build/
